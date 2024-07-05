3572ec6d32dbb4fde9a4dfd1791196dd03107ec8
SSShogunn <aman_singh2021@vitbhopal.ac.in>
2024-07-06 03:01:32 +0530

    added tRPC

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation';
+
+
+const Page = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser();
+    
+    if(!user || !user.id) redirect("/auth-callback?origin-dashboard")
+    
+    return <>{console.log({user})}</>
+
+    
+}
+
+export default Page
