aca5ff295d2a6d0b270da6cd2405a78e2a5213b3
Aman Singh <aman_singh2021@vitbhopal.ac.in>
2024-08-13 14:18:23 +0530

    Update auth-callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,49 +1,46 @@
-"use client";
+"use client"
 
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { trpc } from '../_trpc/client';
-import { Loader2 } from 'lucide-react';
+import { Suspense } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
 
-const Page = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const origin = searchParams.get('origin');
+const AuthCallbackPage = () => {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const origin = searchParams.get('origin')
 
-    const { data, error, isLoading } = trpc.authCallback.useQuery(undefined, {
-        retry: true,
-        retryDelay: 500,
-    });
+    const { data, isLoading, isError, isSuccess } = trpc.authCallback.useQuery();
 
-    useEffect(() => {
-        if (data) {
-            if (data.success) {
-                router.push(origin ? `/${origin}` : '/dashboard');
-            }
+    if (isSuccess) {
+        if (data.success) {
+            router.push(origin ? `/${origin}` : '/dashboard')
         }
+    }
 
-        if (error) {
-            if (error.data?.code === 'UNAUTHORIZED') {
-                router.push('/sign-in');
-            }
-        }
-    }, [data, error, router, origin]);
+    if (isError) {
+        router.push('/sign-in')
+    }
 
     return (
         <div className='w-full mt-24 flex justify-center'>
             <div className='flex flex-col items-center gap-2'>
-                {isLoading ? (
-                    <>
-                        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-                        <h3 className='font-semibold text-xl'>
-                            Setting up your account...
-                        </h3>
-                        <p>You will be redirected automatically.</p>
-                    </>
-                ) : null}
+                {isLoading && <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />}
+                <h3 className='font-semibold text-xl'>
+                    Setting up your account...
+                </h3>
+                <p>You will be redirected automatically.</p>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Page;
+const Page = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <AuthCallbackPage />
+        </Suspense>
+    )
+}
+
+export default Page
